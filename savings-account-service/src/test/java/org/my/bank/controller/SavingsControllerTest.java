7964ce63e5655a0b14ac12b7d48d725f3a83a892
“Manish <manishsricv@gmail.com>
2023-04-09 12:19:24 +0200

    Savings Module: Write unimplemented test cases for http layer

---
+++ savings-account-service/src/test/java/org/my/bank/controller/SavingsControllerTest.java
@@ -0,0 +1,49 @@
+package org.my.bank.controller;
+
+import org.junit.jupiter.api.Test;
+
+class SavingsControllerTest {
+    @Test
+    void openNewSavingAccountWithoutMandatoryFields_Failed() {
+    }
+
+    @Test
+    void openNewSavingAccount_Success() {
+    }
+
+    @Test
+    void depositMoneyWithWrongAccountNumber_Failed() {
+    }
+
+    @Test
+    void depositMoneyWithWrongCustomerId_Failed() {
+    }
+
+    @Test
+    void depositMoney_Success() {
+    }
+
+    @Test
+    void withdrawMoneyWithWrongAccountNumber_Failed() {
+    }
+
+    @Test
+    void withdrawMoneyWithWrongCustomerId_Failed() {
+    }
+
+    @Test
+    void withdrawMoney_Success() {
+    }
+
+    @Test
+    void getTransactionListWithWrongAccount_Failed() {
+    }
+
+    @Test
+    void getTransactionList_Success() {
+    }
+
+    @Test
+    void getBalance_Success() {
+    }
+}
